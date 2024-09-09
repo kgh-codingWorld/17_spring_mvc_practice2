16ecef2e9f42ddbdccdbdc114f205af1e6ce8445
kgh-codingWorld <tto4028@naver.com>
2024-09-09 13:45:44 +0900

    포트폴리오 사전준비중

--- 17_spring_mvc_practice2/src/main/java/com/application/mvc/practice2/controller/LayoutController.java
+++ 17_spring_mvc_practice2/src/main/java/com/application/mvc/practice2/controller/LayoutController.java
@@ -12,4 +12,30 @@ public class LayoutController {
 	public String index() {
 		return "chapter01_thymeleaf/layout/bootstrap/index";
 	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "chapter01_thymeleaf/layout/bootstrap/login";
+	}
+	
+	@GetMapping("/mypage")
+	public String mypage() {
+		return "chapter01_thymeleaf/layout/bootstrap/mypage";
+	}
+	
+	@GetMapping("/introduce")
+	public String introduce() {
+		return "chapter01_thymeleaf/layout/bootstrap/introduce";
+	}
+
+	@GetMapping("/blog")
+	public String blog() {
+		return "chapter01_thymeleaf/layout/bootstrap/blog";
+	}
+	
+	@GetMapping("/cart")
+	public String cart() {
+		return "chapter01_thymeleaf/layout/bootstrap/cart";
+	}
+	
 }
